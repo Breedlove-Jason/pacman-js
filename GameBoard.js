e81b8ccd9999c819c76177ffad0930b91a31fb48
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2022-08-21 15:50:44 -0700

    finished draw gameboard functions

--- GameBoard.js
+++ GameBoard.js
@@ -18,12 +18,30 @@ class GameBoard {
     this.dotCount = 0;
     this.grid = [];
     this.DOMGrid.innerHTML = "";
-    this.DOMGrid.style.cssext = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px)`;
+    this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px)`;
 
     level.forEach((square, index) => {
       const div = document.createElement("div");
-      div.classList.add('square', CLASS_LIST[square]);
+      div.classList.add("square", CLASS_LIST[square]);
       div.style.cssText = `width: ${CELL_SIZE}px; height: ${CELL_SIZE}px;`;
+      this.DOMGrid.appendChild(div);
+      this.grid.push(div);
+
+      if (CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++;
     });
+  }
+  addObject(pos, classes) {
+    this.grid[pos].classList.add(...classes);
+  }
+  removeObject(pos, classes) {
+    this.grid[pos].classList.remove(...classes);
+  }
+
+  objectExist(pos, object) {
+    return this.grid[pos].classList.contains(object);
+  }
+
+  rotateDiv(pos, deg) {
+    this.grid[pos].style.transform = `rotate(${deg}deg)`;
   }
 }
